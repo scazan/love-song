7317b1618d27afd0c56b7a208e77ef38f74b12ee
scazan <scottcazan@gmail.com>
2017-11-08 08:19:36 -0800

    Updates Genetic system

--- app/scripts/Genetic.tsx
+++ app/scripts/Genetic.tsx
@@ -3,49 +3,69 @@ import utils from "./utils";
 
 
 class Genetic {
-	population: Array<number>;
-	goal: Array<number>;
+	population: number[][];
+	goal: number[];
+	scores: number[];
 
-	constructor(input, goal) {
+	constructor(input: number[][], goal: number[]) {
 		this.population = input;
+		this.scores = Array(input.length).fill(0);
 		this.goal = goal;
 	}
 
-	getTotalFitnessRating(input: Array<number>, goal: Array<number>) {
+
+	// Accumulate and return the score for a single collection
+	getTotalFitnessRating(collection: number[], goal: number[]) {
 		let score: number = 0; // lower is better
 
-		for(let i=input.length-1; i >= 0; i--) {
-			score = this.getFitnessRating(input[i], goal[i]);
+		for(let i=collection.length-1; i >= 0; i--) {
+			score = this.getDistance(collection[i], goal[i]);
 		}
 
 		return score;
 	}
 
-	getFitnessRating(input: number, goal: number) {
+	// Returns a numerical distance between an input and a goal
+	getDistance(input: number, goal: number) {
 		let rating: number = goal - input;
 
 		return rating;
 	}
 
-	getNextGeneration(score: number, population: Array<number>) {
-		let newPopulation = [];
 
-		return newPopulation;
+
+
+
+	// Calculate and return the scores for all current collections
+	getPopulationScores(population: number[][], goal: number[]): number[] {
+		let scores = Array(population.length).fill(0);
+
+		for(let i=(population.length-1); i>=0; i--) {
+			scores[i] = this.getTotalFitnessRating(population[i], goal);
+		}
+
+		return scores;
 	}
 
-	getNextState(state: Array<number>) {
+	getNextGeneration(score: number, population: number[][]) {
+		let newGeneration = [];
 
-		let fitnessScore: number = this.getTotalFitnessRating(this.population, this.goal);
+		return newGeneration;
+	}
+
+	getNextState(state: number[]) {
+
+		//let fitnessScore: number = this.getTotalFitnessRating(this.population, this.goal);
 		
-		let nextState: Array<number> = this.getNextGeneration(fitnessScore, this.population);
+		//let nextState: number[] = this.getNextGeneration(fitnessScore, this.population);
 
-		return nextState;
+		//return nextState;
 	}
 
 	asPattern() {
 		let self = this;
 
-		return function* asPattern(initialState: Array<number>) {
+		return function* asPattern(initialState: number[]) {
 			/*self.lastState = initialState;*/
 
 			while(true) {
